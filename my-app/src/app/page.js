bb62ef8f0410906e75cd6dbe4e7b009d3fd08ce4
Gustavo Lima de Carvalho <109549872+limagustavo2200@users.noreply.github.com>
2024-10-22 17:49:05 -0300

    first commit

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -1,10 +1,28 @@
+'use client';
 import Image from "next/image";
 import styles from "./page.module.css";
+import { useState } from "react";
+import { CiMenuBurger } from "react-icons/ci";
+import { CgClose } from "react-icons/cg";
+import Menu from "@/components/Menu/Menu";
+
+
 
 export default function Home() {
+  const [menuAberto, setMenuAberto] = useState(false)
+  
+  function atualizarMenu() {
+    setMenuAberto(!menuAberto)
+  }
+  
   return (
-    <div>
-
-    </div>
+    <main className={styles.container}>
+      <header className={styles.topo}>
+        <button className={styles.botao} onClick={atualizarMenu}>{menuAberto ? <CgClose /> : <CiMenuBurger />}</button>
+      </header>
+      {menuAberto && (
+        <Menu />
+      )}
+    </main>
   );
 }
